7c30a061e0e8d041c5201b60c78ad7efbecd67f1
yf <--->
2024-06-22 18:17:15 +0800

    dotfiles: 修改powershell配置

--- dotfiles/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,37 +1,12 @@
-###############################################################################
-#                                                                             #
-#                                  colorscheme                                #
-#                                                                             #
-###############################################################################
+############################### colorscheme
 
 $PSStyle.FileInfo.Directory = "`e[34;1m"
 
-###############################################################################
-#                                                                             #
-#                                    alias                                    #
-#                                                                             #
-###############################################################################
+############################### function
 
-# 移除powershell自带的别名
-Remove-Alias -Name "sl" -Force # 默认是Set-Location
-Remove-Alias -Name "where" -Force # 默认是Where-Object，移除后使用系统自带的where程序
-
-Set-Alias -Name whereis -Value Get-Command
-Set-Alias -Name which -Value Get-Command
-
-Set-Alias -Name lg -Value lazygit
-Set-Alias -Name ld -Value lazydocker
-
-Set-Alias -Name jdks -Value "$Env:USERPROFILE/space/scripts/jdk_select.ps1"
-Set-Alias -Name mvns -Value "$Env:USERPROFILE/space/scripts/maven_select.ps1"
-
-###############################################################################
-#                                                                             #
-#                                  function                                   #
-#                                                                             #
-###############################################################################
-
+function ls { Get-ChildItem | Format-Table -HideTableHeaders -AutoSize }
 function ll { Get-ChildItem | Format-Table -HideTableHeaders -AutoSize }
+
 function lla {
     Get-ChildItem -Attributes Archive,Compressed,Device,Directory,Encrypted,Hidden,IntegrityStream,Normal,NoScrubData,NotContentIndexed,Offline,ReadOnly,ReparsePoint,SparseFile,System,Temporary | Format-Table -HideTableHeaders -AutoSize 
 }
@@ -54,30 +29,36 @@ function sudocmd { sudo alacritty.exe --command cmd }
 # 管理员方式打开powershell
 function sudopwsh { sudo alacritty.exe --command pwsh }
 
-###############################################################################
-#                                                                             #
-#                                   keymap                                    #
-#                                                                             #
-###############################################################################
+############################### alias
+
+# 移除powershell自带的别名
+Remove-Alias -Name "sl" -Force # 默认是Set-Location
+Remove-Alias -Name "ls" -Force # 
+Remove-Alias -Name "where" -Force # 默认是Where-Object，移除后使用系统自带的where程序
+
+Set-Alias -Name whereis -Value Get-Command
+Set-Alias -Name which -Value Get-Command
+
+Set-Alias -Name lg -Value lazygit
+Set-Alias -Name ld -Value lazydocker
+
+Set-Alias -Name jdks -Value "$Env:USERPROFILE/space/scripts/jdk_select.ps1"
+Set-Alias -Name mvns -Value "$Env:USERPROFILE/space/scripts/maven_select.ps1"
+Set-Alias -Name vim -Value "$Env:USERPROFILE/space/scripts/vim.cmd"
+############################### keymap
 
 Set-PSReadLineOption -EditMode Emacs # 可选的，选择编辑模式
 
-###############################################################################
-#                                                                             #
-#                                     env                                     #
-#                                                                             #
-###############################################################################
+Set-PSReadlineOption -BellStyle None # 禁用铃声
+
+############################### env
 
 # fzf选项配置
 Set-Item -Path 'Env:\FZF_DEFAULT_OPTS' -Value "--height 95% --layout=reverse --prompt='❯ ' --info=inline:' ' --preview-window=down:60%"
 # lf配置
 Set-Item -Path 'Env:\EDITOR' -Value "nvim"
 
-###############################################################################
-#                                                                             #
-#                                    plugin                                   #
-#                                                                             #
-###############################################################################
+############################### plugins
 
 # 安装 z 插件
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
