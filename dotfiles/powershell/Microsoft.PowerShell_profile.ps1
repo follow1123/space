ccc266c0a2fdd9407dc6f91c8790642f43469969
yf <--->
2024-10-16 23:32:40 +0800

    powershell: 删除无效配置，添加查询子进程方法

--- dotfiles/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,10 +4,6 @@ $PSStyle.FileInfo.Directory = "`e[34;1m"
 
 ############################### function
 
-function lla {
-    Get-ChildItem -Attributes Archive,Compressed,Device,Directory,Encrypted,Hidden,IntegrityStream,Normal,NoScrubData,NotContentIndexed,Offline,ReadOnly,ReparsePoint,SparseFile,System,Temporary | Format-Table -HideTableHeaders -AutoSize 
-}
-
 # 使用管理员方式打开
 function sudo {
     param(
@@ -18,11 +14,30 @@ function sudo {
     Start-Process -FilePath $Command -ArgumentList $Arguments -Verb RunAs
 }
 
-# 管理员方式打开cmd
-function sudocmd { sudo alacritty.exe --command cmd }
+# 根据进程id查找其父进程
+function findParentProcess {
+	param (
+		[int]$ProcessId,
+		[int]$Depth = 5
+	)
 
-# 管理员方式打开powershell
-function sudopwsh { sudo alacritty.exe --command pwsh }
+	if (-not $ProcessId) {
+		Write-Output 'no process id'
+		return
+	}
+
+	$processArray=@($null) * $Depth
+
+	for ($i = 0; $i -lt $Depth; $i++) {
+		$processArray[$i] = Get-CimInstance Win32_Process -Filter "ProcessId = $ProcessId"
+		if ($processArray[$i].ProcessId -eq 0) {
+			break
+		}
+		$ProcessId = $processArray[$i].ParentProcessId
+	}
+
+	Write-Output $processArray
+}
 
 ############################### alias
 
@@ -30,27 +45,22 @@ function sudopwsh { sudo alacritty.exe --command pwsh }
 Remove-Alias -Name "sl" -Force # 默认是Set-Location
 Remove-Alias -Name "where" -Force # 默认是Where-Object，移除后使用系统自带的where程序
 
-Set-Alias -Name whereis -Value Get-Command
-Set-Alias -Name which -Value Get-Command
-
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name ld -Value lazydocker
 
 Set-Alias -Name jdks -Value "$Env:USERPROFILE/space/scripts/jdk_select.ps1"
 Set-Alias -Name mvns -Value "$Env:USERPROFILE/space/scripts/maven_select.ps1"
 Set-Alias -Name vim -Value "$Env:USERPROFILE/space/scripts/vim.cmd"
+
 ############################### keymap
 
 Set-PSReadLineOption -EditMode Emacs # 可选的，选择编辑模式
-
 Set-PSReadlineOption -BellStyle None # 禁用铃声
 
 ############################### env
 
 # fzf选项配置
 Set-Item -Path 'Env:\FZF_DEFAULT_OPTS' -Value "--height 95% --layout=reverse --prompt='❯ ' --info=inline:' ' --preview-window=down:60%"
-# lf配置
-Set-Item -Path 'Env:\EDITOR' -Value "nvim"
 
 ############################### plugins
 
