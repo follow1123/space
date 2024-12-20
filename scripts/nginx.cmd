661961828634ee6ea426f482d6c0fb9b34fb3808
yf <--->
2024-12-21 00:15:44 +0800

    scripts: 更新nginx启动脚本

--- scripts/nginx.cmd
+++ scripts/nginx.cmd
@@ -1,11 +1,37 @@
 @echo off
 
-cd C:\Users\yf\space\env\nginx\1.24.0
+setlocal
 
-if "%1" == "" (goto :start_nginx)
-nginx.exe %*
+set nginx_home=%USERPROFILE%\space\packages\nginx\current
+set nginx_pid_file=%nginx_home%\logs\nginx.pid
+set nginx_cmd=%nginx_home%\nginx.exe
 
-goto :EOF
+if "%1" == "" (
+	call :start_nginx
+) else (
+	start %nginx_cmd% -p %nginx_home% %*
+)
+
+endlocal
+
+goto :eof
+
+:is_running
+for /f "tokens=2 delims=," %%i ^
+in ('tasklist /nh /fi "imagename eq nginx.exe" /fo csv') ^
+do set nginx_pid=%%i
+
+if not defined nginx_pid (exit /b 1)
+if %nginx_pid% neq "" (exit /b 0)
+goto :eof
 
 :start_nginx
-start nginx.exe
+call :is_running
+if %ERRORLEVEL% equ 0 (
+	echo nginx is running
+	pause
+	goto :eof
+)
+
+start %nginx_cmd% -p %nginx_home%
+goto :eof
